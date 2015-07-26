43ccf9b0337fbde061b81464f8e07f2b4a57adc4
Barb <birons89@gmail.com>
2015-07-25 21:02:11 -0400

    added favicon

--- resume.js
+++ resume.js
@@ -1,6 +1,11 @@
 
 if (Meteor.isClient) {
   document.title = "Barbara Irons";
+  (function() { var link = document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'favicon-cogs.ico';
+    document.getElementsByTagName('head')[0].appendChild(link); }());
 
   Template.resume.rendered = function () {
     var toggleFloatingMenu = function() {
